075034fbe7c55b0b7f265626e888fc62c7793d30
arch7tect <arch7tect@gmail.com>
2021-10-25 07:04:11 +0300

    replace sample

--- simple/src/main/java/ru/neoflex/spark/jobs/SimpleJob2.java
+++ simple/src/main/java/ru/neoflex/spark/jobs/SimpleJob2.java
@@ -6,6 +6,7 @@ import org.apache.spark.sql.*;
 import org.apache.spark.storage.StorageLevel;
 import ru.neoflex.spark.base.ISparkJob;
 import ru.neoflex.spark.base.SparkJobBase;
+import ru.neoflex.spark.base.Utils;
 
 import java.util.List;
 import java.util.Map;
@@ -19,8 +20,7 @@ public class SimpleJob2 extends SparkJobBase {
     public void run(SparkSession spark, JavaSparkContext sc, Map<String, String> jobParameters) throws Exception {
         List<Integer> data = IntStream.range(0, 100).boxed().collect(Collectors.toList());
         spark.createDataset(data, Encoders.INT()).coalesce(3).toDF("id").createTempView("is");
-        String fileName = jobParameters.getOrDefault("file", "simple.parquet");
-        String path = "/data/" + fileName;
+        String path = Utils.replace("/data/${file:-simple.parquet}", jobParameters);
         String sql = formatResource("sql/addName.sql", jobParameters);
         Dataset<Row> dfSql = spark.sql(sql);
         dfSql.persist(StorageLevel.MEMORY_AND_DISK());
