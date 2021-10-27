6cc26a1deb4bd5ec7259f6acd83166c3c39636b4
arch7tect <arch7tect@gmail.com>
2021-10-27 10:49:47 +0300

    default params in query

--- simple/src/main/resources/sql/addName.sql
+++ simple/src/main/resources/sql/addName.sql
@@ -1,6 +1,7 @@
 select
-       ID,
+       id,
+       uuid() as uuid,
        concat('name_', ID%7) as name,
        concat('description_', ID%9) as description
 from is
-where ID < ${num}
+where ID < ${num:-999999}
