bd1ceebaeec189950001ef1b84c2e546c8dd10d7
arch7tect <arch7tect@gmail.com>
2021-10-28 13:35:31 +0300

    spark.range

--- simple/src/main/java/ru/neoflex/spark/jobs/SaveAsTableJob.java
+++ simple/src/main/java/ru/neoflex/spark/jobs/SaveAsTableJob.java
@@ -2,35 +2,29 @@ package ru.neoflex.spark.jobs;
 
 import com.google.auto.service.AutoService;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
 import ru.neoflex.spark.base.ISparkJob;
 import ru.neoflex.spark.base.SparkJobBase;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @AutoService(ISparkJob.class)
 public class SaveAsTableJob extends SparkJobBase {
-
-    protected Dataset<Row> createIDs(SparkSession spark, int from, int to) {
-        List<Integer> data = IntStream.range(from, to).boxed().collect(Collectors.toList());
-        return spark.createDataset(data, Encoders.INT()).toDF("id");
-    }
 
     @Override
     public void run(SparkSession spark, JavaSparkContext sc, Map<String, String> jobParameters) throws Exception {
         info("bucketingEnabled: %b", spark.sessionState().conf().bucketingEnabled());
         //spark.conf().set("spark.sql.autoBroadcastJoinThreshold", -1);
 
-        createIDs(spark, 0, 1000000).createOrReplaceTempView("t1");
+        spark.range(5000000).createOrReplaceTempView("t1");
         String sql1 = formatResource("sql/addName.sql", "tempTable", "t1");
         spark.sql(sql1).orderBy("uuid").write().mode(SaveMode.Overwrite).bucketBy(100, "id").sortBy("id").saveAsTable("names_b1");
         Dataset<Row> df1 = spark.table("names_b1");
 
-        createIDs(spark, 0, 1000000).createOrReplaceTempView("t2");
+        spark.range(5000000).createOrReplaceTempView("t2");
         String sql2 = formatResource("sql/addName.sql", "tempTable", "t2");
         spark.sql(sql2).orderBy("uuid").write().mode(SaveMode.Overwrite).bucketBy(100, "id").sortBy("id").saveAsTable("names_b2");
         Dataset<Row> df2 = spark.table("names_b2");
