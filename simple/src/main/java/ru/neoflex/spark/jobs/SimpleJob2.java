a0ec09aba69041806f2c30c7de6d5f4c72b082c9
arch7tect <arch7tect@gmail.com>
2021-10-29 11:45:24 +0300

    use spark.range

--- simple/src/main/java/ru/neoflex/spark/jobs/SimpleJob2.java
+++ simple/src/main/java/ru/neoflex/spark/jobs/SimpleJob2.java
@@ -2,25 +2,23 @@ package ru.neoflex.spark.jobs;
 
 import com.google.auto.service.AutoService;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.storage.StorageLevel;
 import ru.neoflex.spark.base.ISparkJob;
 import ru.neoflex.spark.base.SparkJobBase;
 import ru.neoflex.spark.base.Utils;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @AutoService(ISparkJob.class)
 public class SimpleJob2 extends SparkJobBase {
 
     @Override
     public void run(SparkSession spark, JavaSparkContext sc, Map<String, String> jobParameters) throws Exception {
-        List<Integer> data = IntStream.range(0, 100).boxed().collect(Collectors.toList());
-        spark.createDataset(data, Encoders.INT()).coalesce(3).toDF("id").createTempView("is");
+        spark.range(100).coalesce(3).createOrReplaceTempView("is");
         String sql = formatResource("sql/addName.sql", jobParameters);
         Dataset<Row> dfSql = spark.sql(sql);
         dfSql.persist(StorageLevel.MEMORY_AND_DISK());
